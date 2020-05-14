e81d51850733633a39af49ca621931371fa4205e
Freelancer0121 <46292091+bsjigi@users.noreply.github.com>
2020-05-14 19:26:58 +0900

    Delete functionSample04.py

--- Day07/Function/functionSample04.py
+++
@@ -1,8 +0,0 @@
-height = int(input('키를 입력하세요 : '))
-weight = int(input('몸무게를 입력하세요 :'))
-def BMI(height, weight):
-    Meter = height/100
-    bmi = weight/(Meter*Meter)
-    return bmi
-
-print(BMI(height,weight))
